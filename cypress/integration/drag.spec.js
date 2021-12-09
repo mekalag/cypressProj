cd456e08639f968fcb0f23cd4042b5f63bcb86bf
kalavathig1 <kalavathi.golla@softwareag.com>
2021-12-09 11:27:25 +0100

    files added to demonstrate drag of a element bottom right

---
+++ cypress/integration/drag.spec.js
@@ -0,0 +1,36 @@
+describe('My First Test', () => {
+
+    before(() => {
+        cy.visit(Cypress.env('baseUrl'))
+        cy.contains('Agree and proceed').click()
+        //cy.wait(10000)
+        //cy.clearCookies()
+        cy.get('#user').type('testuser')
+        cy.get('#password').type('Test@1234')
+        cy.contains('Log in').click()
+    })
+
+    //after hook is needed to delete Applications widget.
+    // Incase if the test fails at other place, and application widget is present then manually needs to be deleted before rerunning
+    after(() => {
+        cy.wait(10000)
+        cy.get('.applications .btnIcon').click()
+        cy.get('[title="Remove widget"]').click()
+    })
+
+    it('finds the content "type"', () => {
+        cy.get('#page-toolbar').contains('Add widget').click()
+        cy.contains('li', 'Add widget').click()
+        cy.wait(2000)
+        cy.get('[placeholder="Search…"]').type('Applications')
+        cy.contains('Applications').click()
+        cy.contains('Save').click({force: true})
+        //y = cy.get('#cdk-drop-list-1').getYcoordinate();
+        cy.get('.applications > .cdk-drag')
+            //.move({ deltaX: 700, deltaY: 700, force: true})
+            .trigger('mousedown', { which: 1 })
+            .trigger('mousemove', {clientX: 700, clientY: 500})
+            .trigger('mouseup', {force: true})
+    })
+})
+
